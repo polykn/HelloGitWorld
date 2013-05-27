f4501d807eea22a7e0373d54ff220dec76d37143
poly-kn-win8 <kn@poly-control.dk>
2013-05-27 15:01:18 +0200

    Init

---
+++ src/HelloGitWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HelloGitWorld.c
+ Author      : KN
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Jamen goddag :-) !!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
